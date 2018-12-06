41f94c4336d6395c217de3a8a6b6a2ac92be5d43
Leizl Samano <leizl.samano@gmail.com>
2018-12-06 22:45:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,21 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(yaml)
+  yaml = YAML.load_file('lib/emoticons.yml')
+  emote_library = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  yaml.each do |name_keys, emoticons_hash|
+    emote_library["get_meaning"].merge!({emoticons_hash[1] => name_keys})
+    emote_library["get_emoticon"].merge!({emoticons_hash[0] => emoticons_hash[1]})
+  end
+  emote_library
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(yaml, emoticon)
   # code goes here
 end
 
